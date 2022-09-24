334d9280a73a22278acc24d92d9093c580b41356
Sn2910 <sneha.shaw29@gmail.com>
2022-09-24 11:11:19 +0200

    Db-operation working

--- controllers/db_operations.js
+++ controllers/db_operations.js
@@ -1,40 +1,200 @@
 const { Pool } = require("pg");
 const pool = new Pool({
-    user: process.env.PG_USER,
-    host: process.env.PG_HOST,
-    database: process.env.PG_DATABASE,
-    password: process.env.PG_PASSWORD,
-    port: process.env.PG_PORT,
-    ssl: true,
-    ssl: { rejectUnauthorized: false },
+  user: process.env.PG_USER,
+  host: process.env.PG_HOST,
+  database: process.env.PG_DATABASE,
+  password: process.env.PG_PASSWORD,
+  port: process.env.PG_PORT,
+  ssl: true,
+  ssl: { rejectUnauthorized: false },
+});
+
+/**
+ * This is a generic PostgreSQL database select fetch function
+ * @params sqlQuery is the complete string
+ * @returns result of the database query
+ * @todo refacoring: fields contains the fields to return, idField represents the field name in the where clause, id represents the identificator to look for. The commented code is ready to run
+ * @date 2022-09-23
+ */
+async function getSingleData(sqlQuery, destinationId) {
+  // const idField = `destination_id`;
+  // const fields = [];
+  // const fieldString = fields.length === 0 ? "*" : "";
+  // fieldString !== "*" &&
+  //   fields.forEach(field =>
+  //     fieldString === ""
+  //       ? fieldString.concat("'", field, "'")
+  //       : fieldString.concat(fieldString, ", '", field, "'")
+  //   );
+
+  // const queryString = idField !== "" ? `SELECT ${fieldString} FROM '${table}' WHERE ${idField} = $1;` : `SELECT ${fieldString} FROM '${table}'`;
+
+  let data;
+  await pool
+    .connect()
+    .then(async (client) => {
+      // return client.query(sqlQuery, [destinationId]).then(res => {
+      if (destinationId === null)
+        return client.query(sqlQuery).then((res) => {
+          client.release();
+          data = res.rows;
+        });
+      return client.query(sqlQuery, [destinationId]).then((res) => {
+        client.release();
+        data = res.rows;
+      });
+    })
+    .catch((e) => {
+      console.log(e.stack);
+    });
+  return data;
+}
+
+/**
+ * This function calls getSingleData() for quering the hotel table of the database
+ *  @params destinationId represents the id to lookup for
+ *  @return json object
+ */
+async function getDestinationHotels(destinationId) {
+  //TODO:  Select data from Hotels table by destination id .Return Data as an Array
+  return await getSingleData(
+    'SELECT * FROM "hotels" WHERE  destination_id = $1;',
+    destinationId
+  );
+
+  // return pool
+  //   .query("SELECT * FROM hotels WHERE  destination_id =$1;", [destinationId])
+  //   .then((data) => {
+  //     return data.rows;
+  //   });
+}
+
+/**
+ * This function calls getSingleData() for quering the shops table of the database
+ *  @params destinationId represents the id to lookup for
+ *  @return json object
+ */
+async function getDestinationShops(destinationId) {
+  //TODO:  Select data from Hotels table by destination id .Return Data as an Array
+  return await getSingleData(
+    'SELECT * FROM "shops" WHERE  destination_id =$1;',
+    destinationId
+  );
+}
+
+/**
+ * This function calls getSingleData() for quering the restaurants table of the database
+ *  @params destinationId represents the id to lookup for
+ *  @return json object
+ */
+async function getDestinationRestaurants(destinationId) {
+  //TODO:  Select data from Restaurants table by destination id .Return Data as an Array
+  return await getSingleData(
+    'SELECT * FROM "restaurants" WHERE  destination_id =$1;',
+    destinationId
+  );
+  /*  return pool
+    .query("SELECT * FROM restaurants WHERE  destination_id =$1;", [
+      destinationId,
+    ])
+    .then((data) => {
+      return data.rows;
+    }); */
+}
+
+/**
+ * This function calls getSingleData() for quering the destinations table of the database
+ *  @params no params
+ *  @return json object
+ */
+async function getDestination() {
+  /*  return pool.query(`SELECT * FROM destinations;`).then((data) => {
+    return data.rows;
+  }); */
+  return await getSingleData('SELECT * FROM "destinations";', null);
+}
+
+/**
+ * This function calls getSingleData() for quering the destinations table of the database
+ *  @params id represents the id to lookup for
+ *  @return json object
+ *  @todo outsourcing of everything except the database functionallity
+ */
+async function getOneDestination(id) {
+  //TODO:  Select data for one Destination by destination id .Return Data as an Array.store in destinationObj
+
+  /* pool.query("SELECT * FROM destinations WHERE id =$1;", [id]).then((data) => {
+    return data.rows;
+  }); */
+  getSingleData('SELECT * FROM "destinations" WHERE id =$1;', id);
+  const destinationObj = {
+    id: id,
+  };
+  const restaurants = await getDestinationRestaurants(destinationObj.id);
+  console.log(restaurants);
+  const hotels = await getDestinationHotels(destinationObj.id);
+  const shops = await getDestinationShops(destinationObj.id);
+  destinationObj.restaurants = restaurants;
+  destinationObj.hotels = hotels;
+  destinationObj.shops = shops;
+  console.log(destinationObj);
+
+  return destinationObj;
+}
+
+/**
+ * This function ...
+ *  @params ...
+ *  @return ...
+ *  @todo ...
+ */
+async function getAssets() {
+  //TODO:  Select data from Hotels table by destination id .Return Data as an Array
+  /* return (dbData = pool.query(`SELECT * FROM assets;`).then((data) => {
+    return data.rows;
+  })); */
+  return await getSingleData('SELECT * FROM "assets";', null);
+}
+
+async function getBlogs() {
+  return pool.query(`SELECT * FROM blogs;`).then((data) => {
+    return data.rows;
   });
-  
-
-function getDestinationHotels(destinationId){
-//TODO:  Select data from Hotels table by destination id .Return Data as an Array
 }
-function getDestinationRestaurants(destinationId){
- //TODO:  Select data from Restaurants table by destination id .Return Data as an Array
+async function getOneBlog(id) {
+  return pool
+    .query(
+      `
+    SELECT * FROM blogs WHERE id=$1;
+    `,
+      [id]
+    )
+    .then((data) => {
+      return data.rows;
+    });
 }
-async function getOneDestination(destinationId){
-    //TODO:  Select data for one Destination by destination id .Return Data as an Array.store in destinationObj
-    const destinationObj = {
-        
-    };
-    const restaurants = await getDestinationRestaurants(destinationObj.id)
-    const hotels = await getDestinationHotels(destinationObj.id)
-    destinationObj.restaurants = restaurants;
-    destinationObj.hotels =hotels;
-    return destinationObj;
-   }
-   function getDestination(){
-    return pool.query(`SELECT * FROM destinations;`)
-    .then((data)=>{ return data.rows})  
+async function postBlog() {
+  return pool
+    .query(
+      `
+        INSERT INTO blogs (user_name, blog_date, title, rich_text)
+        values ($1, $2, $3, $4) returning *;
+        `,
+      [req.body.userName, req.body.blogDate, req.body.title, req.body.richText]
+    )
+    .then((data) => {
+      return data.rows;
+    });
+}
 
-   }
-   module.exports = {
-    getDestinationHotels,
-    getDestinationRestaurants,
-    getOneDestination,
-    getDestination
-   }
+module.exports = {
+  getDestinationHotels,
+  getDestinationRestaurants,
+  getOneDestination,
+  getDestination,
+  getDestinationShops,
+  getAssets,
+  getBlogs,
+  getOneBlog,
+  postBlog,
+};
--- index.js
+++ index.js
@@ -1,62 +1,78 @@
-
 const dotenv = require("dotenv");
 dotenv.config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
+
 app.use(express.json());
 app.use(cors());
-app.use(express.static('public'));
+app.use(express.static("public"));
 
-//TODO : Import db operationsa controller functions here
+//TODO : Import db operations controller functions here
 const {
   getDestinationHotels,
   getDestinationRestaurants,
   getOneDestination,
-  getDestination
- } = require('./controllers/db_operations')
+  getDestination,
+  getDestinationShops,
+  getAssets,
+  getBlogs,
+  getOneBlog,
+  postBlog,
+} = require("./controllers/db_operations");
+
 const port = process.env.PORT || 3000;
 // TODO: Move to db_operations.js
 
 app.get("/", (req, res) => {
   res.send("Testing");
 });
-app.get("/api/destinations",(req, res) => {
-    getDestination()
-    .then((destinations) => {
-      res.json(destinations)
-    })
-    .catch((err) => {
-      res.status(400).send({
-        error: err.message,
-      });
-    });
-})
 
-/* app.get("/api/destinations/:id",(req, res) => {
-  // TODO: Replace Db operations with call to getDestination()
-  const {id} = req.params;
-  pool
-    .query('SELECT * FROM destinations WHERE id =$1;',[id])
-    .then((data) => {
-      res.status(201).send(data.rows);
+app.get("/api/destinations", (req, res) => {
+  getDestination()
+    .then((destinations) => {
+      res.json(destinations);
     })
     .catch((err) => {
       res.status(400).send({
         error: err.message,
       });
     });
-})
+});
+
+app.get("/api/destinations/:id", (req, res) => {
+  // TODO: Replace Db operations with call to getDestination()
+  const { id } = req.params;
+
+  getOneDestination(id)
+    .then((destination) => {
+      res.json(destination);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        error: err.message,
+      });
+    });
+});
+4;
+app.get("/api/assets", (req, res) => {
+  // TODO: Replace Db operations with call to getDestination()
+
+  getAssets()
+    .then((assets) => {
+      res.json(assets);
+    })
+    .catch((err) => {
+      res.status(400).send({
+        error: err.message,
+      });
+    });
+});
 
 app.get("/api/blog", (req, res) => {
-  pool
-    .query(
-      `
-    SELECT * FROM blogs;
-    `
-    )
-    .then((data) => {
-      res.status(201).send(data.rows);
+  getBlogs()
+    .then((blogs) => {
+      res.json(blogs);
     })
     .catch((err) => {
       res.status(400).send({
@@ -67,15 +83,9 @@ app.get("/api/blog", (req, res) => {
 
 app.get("/api/blog/:id", (req, res) => {
   const { id } = req.params;
-  pool
-    .query(
-      `
-    SELECT * FROM blogs WHERE id=$1;
-    `,
-      [id]
-    )
-    .then((data) => {
-      res.json(data.rows);
+  getOneBlog(id)
+    .then((blog) => {
+      res.json(blog);
     })
     .catch((err) => {
       res.status(400).send({
@@ -85,16 +95,9 @@ app.get("/api/blog/:id", (req, res) => {
 });
 
 app.post("/api/blog", (req, res) => {
-  pool
-    .query(
-      `
-    INSERT INTO blogs (user_name, blog_date, title, rich_text)
-    values ($1, $2, $3, $4) returning *;
-    `,
-      [req.body.userName, req.body.blogDate, req.body.title, req.body.richText]
-    )
-    .then((data) => {
-      res.status(201).send(data.rows);
+  postBlog()
+    .then((blog) => {
+      res.json(blog);
     })
     .catch((err) => {
       res.status(400).send({
@@ -102,7 +105,7 @@ app.post("/api/blog", (req, res) => {
       });
     });
 });
- */
+
 console.log(process.env.PG_DATABASE);
 
 app.listen(port, () => console.log(`Server listening at port ${port}`));
