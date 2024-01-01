2908f20c3e226423279e2690dc56e82d28cf79b0
Sn2910 <sneha.shaw29@gmail.com>
2024-01-01 12:54:40 +0100

    database detail updated

--- controllers/db_operations.js
+++ controllers/db_operations.js
@@ -1,15 +1,14 @@
 /*-------- Database Connection------*/
 const { Pool } = require("pg");
 const pool = new Pool({
-  user: process.env.PG_USER,
-  host: process.env.PG_HOST,
-  database: process.env.PG_DATABASE,
-  password: process.env.PG_PASSWORD,
-  port: process.env.PG_PORT,
-  ssl: true,
-  max:50,
-  ssl: { rejectUnauthorized: false },
-});
+  user: process.env.PGUSER,
+  host: process.env.PGHOST,
+  database: process.env.PGDATABASE,
+  password: process.env.PGPASSWORD,
+  port: process.env.PGPORT,
+  ssl: true, 
+  ssl: { rejectUnauthorized: false }
+})
 
 async function makeDatabaseQuery(sqlQuery, params) {
   let data;
